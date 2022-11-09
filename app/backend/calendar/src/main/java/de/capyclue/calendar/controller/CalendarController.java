9e5d67e78681bdc9b4eeae3f45dd1edf6e0ea139
Florian Pilz <code@pilz-mail.de>
2022-11-09 10:59:31 +0100

    fix calendar api path

--- app/backend/calendar/src/main/java/de/capyclue/calendar/controller/CalendarController.java
+++ app/backend/calendar/src/main/java/de/capyclue/calendar/controller/CalendarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/api/user")
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/api/calendar")
 public class CalendarController {
     private final CalendarService calendarService;
 
