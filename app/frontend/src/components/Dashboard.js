754aef9679905c40db9df9b0095af6d28ba9bb1b
Vanessa.Limberger <Vanessa.Limberger@cas.de>
2022-11-18 08:51:30 +0100

    Changed wrong path

--- app/frontend/src/components/Dashboard.js
+++ app/frontend/src/components/Dashboard.js
@@ -3,26 +3,26 @@ import Row from "react-bootstrap/esm/Row";
 import DashboardCard from "./DashboardCard";
 
 import book from '../svg/book.svg';
-import calendar from '/docs/icons/ICON_Calendar_deselected.svg';
-import canteen from '/docs/icons/ICON_Canteen_deselected.svg';
-import dualis from '/docs/icons/ICON_Dualis_deselected.svg';
-import links from '/docs/icons/ICON_Links_deselected.svg';
-import news from '/docs/icons/ICON_News_deselected.svg';
-import contact from '/docs/icons/ICON_Contact_deselected.svg';
-import settings from '/docs/icons/ICON_Settings_deselected.svg'
+import calendar from '../icons/ICON_Calendar_deselected.svg';
+import canteen from '../icons/ICON_Canteen_deselected.svg';
+import dualis from '../icons/ICON_Dualis_deselected.svg';
+import links from '../icons/ICON_Links_deselected.svg';
+import news from '../icons/ICON_News_deselected.svg';
+import contact from '../icons/ICON_Contact_deselected.svg';
+import settings from '../icons/ICON_Settings_deselected.svg'
 
 function Dashboard() {
     return (
         <Container>
             <Row>
                 <DashboardCard title='Moodle'   icon={book} external={true} path='https://moodle.dhbw.de'/>
-                <DashboardCard title='Kalender'    icon={calendar} external={false} path='/calendar'/>
-                <DashboardCard title='Mensa'    icon={canteen} external={false} path='/canteen'/>
+                <DashboardCard title='Kalender'    icon={calendar} external={false} path='../calendar'/>
+                <DashboardCard title='Mensa'    icon={canteen} external={false} path='../canteen'/>
                 <DashboardCard title='Dualis'  icon={dualis} external={true} path='https://dualis.dhbw.de'/>
-                <DashboardCard title='Links'    icon={links} external={false} path='/links'/>
-                <DashboardCard title='News'    icon={news} external={false} path='/news'/>
-                <DashboardCard title='Kontakt'    icon={contact} external={false} path='/contact'/>
-                <DashboardCard title='Einstellungen'    icon={settings} external={false} path='/settings'/>
+                <DashboardCard title='Links'    icon={links} external={false} path='../links'/>
+                <DashboardCard title='News'    icon={news} external={false} path='../news'/>
+                <DashboardCard title='Kontakt'    icon={contact} external={false} path='../contact'/>
+                <DashboardCard title='Einstellungen'    icon={settings} external={false} path='../settings'/>
             </Row>
         </Container>
     );
