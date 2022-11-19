639ba7dbe62665fe5b6f38d695911cc1e565d98d
Yolohans <tomlegel@gmail.com>
2022-11-19 22:46:58 +0100

    added Offcanvas and burger to navibar

--- app/frontend/src/components/Navibar.js
+++ app/frontend/src/components/Navibar.js
@@ -1,5 +1,7 @@
-import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Dashboard from "./Dashboard";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
@@ -7,8 +9,23 @@ import { Link } from 'react-router-dom';
 
 function Navibar() {
     return (
-        <Navbar bg="dark" variant="dark">
-        <Container>
+        <Navbar key={false} bg="dark" variant="dark" expand={false} className="mb-3">
+        <Stack direction="horizontal" gap={3}>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} className="ms-2"/>
+          <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${false}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+              placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+                Navigation
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Dashboard/>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
           <Navbar.Brand>
             <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
               <FontAwesomeIcon icon={faGraduationCap} />
@@ -16,7 +33,7 @@ function Navibar() {
               DHBW Community Dashboard
             </Link> 
           </Navbar.Brand>
-        </Container>
+        </Stack>
       </Navbar>
     );
 }
