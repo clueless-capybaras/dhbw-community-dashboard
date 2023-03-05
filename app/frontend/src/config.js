acadcd78e8bcfedf4e08a25261fe7908a95d4e29
Florian Pilz <code@pilz-mail.de>
2023-03-05 17:39:42 +0100

    add dualis backend url config

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,3 +1,4 @@
 export const baseUrlUser        = '/api/user'       //http://localhost:8080/api/user';
 export const baseUrlCalendar    = '/api/calendar'   //'http://localhost:8081/api/calendar';
 export const baseUrlCanteen     = '/api/canteen'    //'http://localhost:8082/api/canteen';
+export const baseUrlDualis      = '/api/dualis'     //'http://localhost:8083/api/dualis';
