72a298961515a450a3451915194b4429dd53a118
Yolohans <tomlegel@gmail.com>
2023-04-05 23:01:44 +0200

    tested timeGridPlugin and custom views in fullCal

--- app/frontend/src/components/calendar/Calendar.js
+++ app/frontend/src/components/calendar/Calendar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import Container from 'react-bootstrap/esm/Container';
@@ -12,8 +13,8 @@ function Calendar() {
     <Container>
       <h1>Calendar</h1>
       <FullCalendar
-        plugins={[ bootstrap5Plugin, dayGridPlugin, listPlugin ]}
-        initialView="dayGridWeek"
+        plugins={[ bootstrap5Plugin, dayGridPlugin, listPlugin, timeGridPlugin ]}
+        initialView="timeGridSixDay"
         weekends={false}
         themeSystem="bootstrap5"
         height={600}
@@ -21,7 +22,7 @@ function Calendar() {
         headerToolbar={{
           left: 'today prev,next',
           center: 'title',
-          right: 'dayGridMonth,dayGridWeek,listWeek'
+          right: 'dayGridMonth,timeGridSixDay,listWeek'
         }}
         navLinks={true}
         weekNumbers={true}
@@ -30,7 +31,17 @@ function Calendar() {
           month:    'Monat',
           week:     'Woche',
           day:      'Tag',
-          list:     'Liste'
+          list:     'Liste',
+          timeGridSixDay:    '6 Tage'
+        }}
+        views={{
+          timeGridSixDay: {
+            type: 'timeGrid',
+            duration: { days: 6 },
+            buttonText: '10 day',
+            weekends: true,
+            firstDay: 1
+          }
         }}
       />
     </Container>
