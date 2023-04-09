048ec07ecc815115fa35412378ba534434bef08c
Yolohans <tomlegel@gmail.com>
2023-04-09 09:06:35 +0200

    added eventhandler et al for calEvents

--- app/frontend/src/components/calendar/Calendar.js
+++ app/frontend/src/components/calendar/Calendar.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import listPlugin from '@fullcalendar/list'
-import interactionPlugin from '@fullcalendar/interaction'
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 import Container from 'react-bootstrap/esm/Container';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 import {CalendarHttpClientContext} from '../../App';
 
 function Calendar() {
+  const [events, setEvents] = useState([]);
+
+  const handleSelect = (info) => {
+    const { start, end } = info;
+    const eventNamePrompt = prompt("Enter event name");
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {start,
+        end,
+        title: eventNamePrompt,
+        id: uuidv4()}
+      ]);
+    }
+  };
+
   return (
     <Container>
       <h1>Calendar</h1>
       <FullCalendar
-        plugins={[ bootstrap5Plugin, dayGridPlugin, listPlugin, timeGridPlugin ]}
+        editable
+        selectable
+        events={events}
+        select={handleSelect}
+        plugins={[ bootstrap5Plugin, dayGridPlugin, listPlugin, timeGridPlugin, interactionPlugin ]}
         initialView="timeGridSixDay"
         weekends={false}
         themeSystem="bootstrap5"
