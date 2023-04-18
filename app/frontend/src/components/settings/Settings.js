4effc8c2857558cf105025a3beecbdb4a7926e46
leonidehrler <leonid@ehrler-web.de>
2023-04-18 13:51:32 +0200

    first draft of settings navigation structure

--- app/frontend/src/components/settings/Settings.js
+++ app/frontend/src/components/settings/Settings.js
@@ -1,13 +1,52 @@
-import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 
 import CanteenSettings from './CanteenSettings';
 
 function Settings() {
     return(
         <>
-        <h1>Einstellungen</h1>
+        <Tab.Container defaultActiveKey="">
+            <Row>
+                <Col md="3">
+                
+                <h1>Einstellungen</h1>
 
-        <CanteenSettings />
+                <Nav variant="pills" className="flex-column">
+                    <Nav.Link eventKey="account">Account</Nav.Link>
+                    <Nav.Link eventKey="calendar">Kalender</Nav.Link>
+                    <Nav.Link eventKey="canteen">Mensa</Nav.Link>
+                    <Nav.Link eventKey="general">Allgemein</Nav.Link>
+                    <Nav.Link eventKey="about">Über</Nav.Link>
+                </Nav>
+                
+                </Col>
+
+                <Col md="9">
+                
+                <Tab.Content>
+                    <Tab.Pane eventKey="account">
+
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="calendar">
+
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="canteen">
+                        <CanteenSettings />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="general">
+
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="about">
+
+                    </Tab.Pane>
+                </Tab.Content>
+                </Col>
+            </Row>
+        </Tab.Container>
         </>
     );
 }
