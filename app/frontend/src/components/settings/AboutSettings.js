7f9c6f8459fcca68a08a7d154450539fe356ec78
leonidehrler <leonid@ehrler-web.de>
2023-04-18 14:20:45 +0200

    built basic structure for all settings subpages

---
+++ app/frontend/src/components/settings/AboutSettings.js
@@ -0,0 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from 'react-bootstrap/Form';
+
+function AboutSettings(props) {
+    return(
+        <>
+        <h1>Über</h1>
+        
+        <Container>
+            <Row className="mb-3">
+                <Col md="3">
+                
+                </Col>
+                <Col md="9">
+                
+                </Col>
+            </Row>
+        </Container>
+        </>
+    );
+}
+
+export default AboutSettings;
---
+++ app/frontend/src/components/settings/AccountSettings.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Form from 'react-bootstrap/Form';
+
+function AccountSettings(props) {
+
+    return(
+        <>
+        <h1>Account Einstellungen</h1>
+        
+        <Container>
+            <Row className="mb-3">
+                <Col md="3">
+                
+                </Col>
+                <Col md="9">
+                
+                </Col>
+            </Row>
+        </Container>
+        </>
+    );
+}
+
+export default AccountSettings;
