5e160bbe6d641e27b4cdf42d5e4f451522ae5a1d
leonidehrler <leonid@ehrler-web.de>
2023-04-18 10:22:34 +0200

    created file for settings

---
+++ app/frontend/src/components/settings/Settings.js
@@ -0,0 +1,15 @@
+import Accordion from 'react-bootstrap/Accordion';
+
+import CanteenSettings from './CanteenSettings';
+
+function Settings() {
+    return(
+        <>
+        <h1>Einstellungen</h1>
+
+        <CanteenSettings />
+        </>
+    );
+}
+
+export default Settings;
