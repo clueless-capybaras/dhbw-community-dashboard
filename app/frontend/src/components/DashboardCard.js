0d7478848ea7bf3a96d49049b54f93755900b33d
Vanessa.Limberger <Vanessa.Limberger@cas.de>
2023-05-05 13:28:59 +0200

    not working draft

--- app/frontend/src/components/DashboardCard.js
+++ app/frontend/src/components/DashboardCard.js
@@ -2,11 +2,12 @@ import { useNavigate } from 'react-router';
 
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import handleClose from './Navibar';
 
 function DashboardCard(props) {
     const navigate = useNavigate();
     return (
-        <Col /*xs={2} s={2} md={2} lg={4}*/ xl={props.colWidth}>
+        <Col /*xs={2} s={2} md={2} lg={4}*/ xl={props.colWidth} onClick={handleClose}>
             <Card /*bg='light'*/ border='0' className='m-2 mt-3' style={{ width: 'auto', cursor: 'pointer', padding: '0' }} onClick={()=> (props.external)?window.open(props.path, props.title):navigate(props.path)}>
                 <Card.Img style={{width: '10rem', margin: 'auto'}} variant="bottom" src={props.icon} />
                 <Card.Body>
--- app/frontend/src/components/Navibar.js
+++ app/frontend/src/components/Navibar.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Stack from 'react-bootstrap/Stack';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -8,33 +10,37 @@ import logo from "../icons/LOGO_DHBWCD.svg";
 import { Link } from 'react-router-dom';
 
 function Navibar() {
-    return (
-        <Navbar key={false} bg="dark" variant="dark" expand={false} className="mb-3">
-        <Stack direction="horizontal" gap={3}>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} className="ms-2"/>
-          <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${false}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
-              placement="start"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
-                Navigation
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Dashboard colWidth='6'/>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-          <Navbar.Brand>
-            <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
-              <img src={logo} alt="Logo" style={{height: '30px', width: '30px'}} className="d-inline-block align-top"/>
-              {' '}
-              DHBW Community Dashboard
-            </Link> 
-          </Navbar.Brand>
-        </Stack>
-      </Navbar>
-    );
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  return (
+      <Navbar key={false} bg="dark" variant="dark" expand={false} className="mb-3">
+      <Stack onClick={handleShow} direction="horizontal" gap={3}>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} className="ms-2"/>
+        <Navbar.Offcanvas
+            show={show}
+            id={`offcanvasNavbar-expand-${false}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+            placement="start"
+        >
+          <Offcanvas.Header onClick={handleClose} closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+              Navigation
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Dashboard colWidth='6'/>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+        <Navbar.Brand>
+          <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+            <img src={logo} alt="Logo" style={{height: '30px', width: '30px'}} className="d-inline-block align-top"/>
+            {' '}
+            DHBW Community Dashboard
+          </Link> 
+        </Navbar.Brand>
+      </Stack>
+    </Navbar>
+  );
 }
 export default Navibar;
