7585616ca70fbcee215886c1f76c0052dbe4965a
Vanessa.Limberger <Vanessa.Limberger@cas.de>
2023-05-10 09:26:38 +0200

    Changed files back that were weirdly commited

--- app/frontend/src/components/DashboardCard.js
+++ app/frontend/src/components/DashboardCard.js
@@ -2,12 +2,11 @@ import { useNavigate } from 'react-router';
 
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import handleClose from './Navibar';
 
 function DashboardCard(props) {
     const navigate = useNavigate();
     return (
-        <Col /*xs={2} s={2} md={2} lg={4}*/ xl={props.colWidth} onClick={handleClose}>
+        <Col /*xs={2} s={2} md={2} lg={4}*/ xl={props.colWidth}>
             <Card /*bg='light'*/ border='0' className='m-2 mt-3' style={{ width: 'auto', cursor: 'pointer', padding: '0' }} onClick={()=> (props.external)?window.open(props.path, props.title):navigate(props.path)}>
                 <Card.Img style={{width: '10rem', margin: 'auto'}} variant="bottom" src={props.icon} />
                 <Card.Body>
