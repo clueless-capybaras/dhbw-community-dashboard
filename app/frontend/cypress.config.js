9d5ed3cc1fb70be3cb12454eaada270e5c82e29b
Yolohans <tomlegel@gmail.com>
2023-05-18 19:24:04 +0200

    cypress mocha reports to github-actions-test add

--- app/frontend/cypress.config.js
+++ app/frontend/cypress.config.js
@@ -5,5 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  }
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: "[status]_[datetime]-[name]-report",
+    timestamp: "longDate",
+    reportDir: 'cypress/results',
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+  },
 })
