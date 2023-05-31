9ad1e76038563d8005c75c0c96134c2ffa96dc3c
Vanessa <vanni.limberger@gmail.com>
2023-05-31 15:20:17 +0200

    finished user backend representation

--- app/backend/user/src/main/java/de/capyclue/user/model/User.java
+++ app/backend/user/src/main/java/de/capyclue/user/model/User.java
@@ -7,7 +7,6 @@ import javax.persistence.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private String id;
 
